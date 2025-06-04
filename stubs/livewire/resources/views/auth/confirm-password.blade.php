2ed6869731b51ccb71ecdcb3891efc2d02ebfe56
Vatsal6288 <kachhadiyavatsal1@gmail.com>
2025-06-04 17:14:54 +0530

    refactor(views): updated auth pages as per laravel version

--- stubs/livewire/resources/views/auth/confirm-password.blade.php
+++ stubs/livewire/resources/views/auth/confirm-password.blade.php
@@ -13,46 +13,64 @@ $customizerHidden = 'customizer-hide';
 @endsection
 
 @section('content')
-<div class="authentication-wrapper authentication-cover">
+<div class="position-relative">
+  <div class="authentication-wrapper authentication-cover">
     <!-- Logo -->
-    <a href="{{ url('/') }}" class="app-brand auth-cover-brand gap-2">
-        <span class="app-brand-logo demo">@include('_partials.macros')</span>
-        <span class="app-brand-text demo text-heading fw-bold">{{ config('variables.templateName') }}</span>
+    <a href="{{ url('/') }}" class="auth-cover-brand d-flex align-items-center gap-3">
+      <span class="app-brand-logo demo">@include('_partials.macros')</span>
+      <span class="app-brand-text demo text-heading fw-semibold">{{ config('variables.templateName') }}</span>
     </a>
     <!-- /Logo -->
     <div class="authentication-inner row m-0">
-        <!-- /Left Text -->
-        <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center p-5">
-            <div class="w-100 d-flex justify-content-center">
-                <img src="{{ asset('assets/img/illustrations/girl-unlock-password-' . $configData['theme'] . '.png') }}" class="img-fluid scaleX-n1-rtl" alt="Login image" width="700" data-app-dark-img="illustrations/girl-unlock-password-dark.png" data-app-light-img="illustrations/girl-unlock-password-light.png">
-            </div>
+      <!-- /Left Text -->
+      <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center justify-content-center p-12 pb-2">
+        <div>
+          <img
+            src="{{ asset('assets/img/illustrations/auth-cover-reset-password-illustration-' . $configData['theme'] . '.png') }}"
+            class="authentication-image-model d-none d-lg-block" alt="auth-model"
+            data-app-light-img="illustrations/auth-cover-reset-password-illustration-light.png"
+            data-app-dark-img="illustrations/auth-cover-reset-password-illustration-dark.png" />
         </div>
-        <!-- /Left Text -->
+        <img src="{{ asset('assets/img/illustrations/tree-3.png') }}" alt="tree"
+          class="authentication-image-tree z-n1" />
+        <img src="{{ asset('assets/img/illustrations/auth-cover-mask-' . $configData['theme'] . '.png') }}"
+          class="scaleX-n1-rtl authentication-image d-none d-lg-block w-75" height="362" alt="triangle-bg"
+          data-app-light-img="illustrations/auth-cover-mask-light.png"
+          data-app-dark-img="illustrations/auth-cover-mask-dark.png" />
+      </div>
+      <!-- /Left Text -->
 
-        <!-- password confirm -->
-        <div class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg p-sm-12 p-6">
-            <div class="w-px-400 mx-auto mt-sm-12 mt-8 pt-5">
-                <h4 class="mb-1">Confirm Password</h4>
-                <p class="mb-6">Please confirm your password before continuing.</p>
-                <form id="twoStepsForm" action="{{ route('password.confirm') }}" method="POST">
-                    @csrf
-                    <div class="mb-6 form-password-toggle">
-                        <label class="form-label" for="password">Enter Password</label>
-                        <div class="input-group input-group-merge @error('password') is-invalid @enderror">
-                            <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
-                            <span class="input-group-text cursor-pointer"><i class="icon-base bx bx-hide"></i></span>
-                        </div>
-                        @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <span class="fw-medium">{{ $message }}</span>
-                        </span>
-                        @enderror
-                    </div>
-                    <button type="submit" class="btn btn-primary d-grid w-100">Confirm Password</button>
-                </form>
+      <!-- password confirm -->
+      <div
+        class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg position-relative py-sm-5 px-12 py-4">
+        <div class="w-px-400 mx-auto pt-12 pt-lg-0">
+          <h4 class="mb-1">Confirm Password</h4>
+          <p class="mb-5">Please confirm your password before continuing.</p>
+          <form id="twoStepsForm" class="mb-5" action="{{ route('password.confirm') }}" method="POST">
+            @csrf
+            <div class="mb-5 form-password-toggle">
+              <div class="input-group input-group-merge @error('password') is-invalid @enderror">
+                <div class="form-floating form-floating-outline">
+                  <input type="password" id="password" class="form-control @error('password') is-invalid @enderror"
+                    name="password"
+                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                    aria-describedby="password" />
+                  <label class="form-label" for="password">Enter Password</label>
+                </div>
+                <span class="input-group-text cursor-pointer"><i
+                    class="icon-base ri ri-eye-off-line icon-20px"></i></span>
+              </div>
+              @error('password')
+              <span class="invalid-feedback" role="alert">
+                <span class="fw-medium">{{ $message }}</span>
+              </span>
+              @enderror
             </div>
+            <button type="submit" class="btn btn-primary d-grid w-100">Confirm Password</button>
+          </form>
         </div>
-        <!-- / password confirm -->
+      </div>
+      <!-- / password confirm -->
     </div>
-</div>
-@endsection
+  </div>
+  @endsection
